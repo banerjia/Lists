11a12f3ffd4384401dbb8cf87f37c06aabc623db
Abhishek Banerji <webdesign.banerji@gmail.com>
2012-09-19 13:19:25 -0700

    Refining Find Helper
    Added "default_scope" to Video model so that all finds search against records that are marked active=1

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,4 +1,6 @@
 class Video < ActiveRecord::Base
+  default_scope :conditions => {:active => true }
+
   belongs_to :site, :counter_cache => true
   attr_accessible :description, :image_url, :rating, :tags, :tags, :title, :url
 end
