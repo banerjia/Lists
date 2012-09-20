20600138b2f44e6b088f2e10ef6c9d01fa06cf9d
Abhishek Banerji <webdesign.banerji@gmail.com>
2012-09-20 12:44:49 -0700

    New Video Controller Added
    Initial work for creating a new entry.

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,4 +1,10 @@
 class VideosController < ApplicationController
   def index
+	@recently_added = Video.find(:all, :conditions => ["active = ? and created_at <= ?", 1, 7.days.ago], :limit => [0,5], :order => "created_at desc")
+	@top_videos = Video.find(:all, :conditions => ["active = ? and rating >= ? ", 1, 5 ], :limit => [0,5], :order => "rating desc")
+  end
+  
+  def new
+	@new_video = Video.new
   end
 end
