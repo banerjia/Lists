4ef8b10d2bddc912ea6b09e1cc7221c72d20438f
Abhishek Banerji <webdesign.banerji@gmail.com>
2013-02-18 20:00:37 -0500

    Bonsai Config Part 2

--- config/initializers/bonsai.rb
+++ config/initializers/bonsai.rb
@@ -1,1 +1,5 @@
-ENV['ELASTICSEARCH_URL'] = 'http://zjfnv9lx:egxemnygttpfpaqd@ash-9154894.us-east-1.bonsai.io'
+ENV['ELASTICSEARCH_URL'] = ENV['BONSAI_URL']
+
+Tire.configure do 
+	url "http://zjfnv9lx:egxemnygttpfpaqd@ash-9154894.us-east-1.bonsai.io" 
+end
