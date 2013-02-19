aa9a91c05a14aef15ef4dbf4e18c5f9df4ef506c
Abhishek Banerji <webdesign.banerji@gmail.com>
2013-02-18 20:30:42 -0500

    Bonsai Final Config

--- config/initializers/bonsai.rb
+++ config/initializers/bonsai.rb
@@ -1,4 +1,4 @@
-ENV['ELASTICSEARCH_URL'] = ENV['BONSAI_URL']
+ENV['ELASTICSEARCH_URL'] = 'http://zjfnv9lx:egxemnygttpfpaqd@ash-9154894.us-east-1.bonsai.io'
 
 Tire.configure do
 	url ENV['ELASTICSEARCH_URL']
